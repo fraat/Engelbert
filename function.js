54bd712a87ef3181d60ed8a751fb151ffe03945d
fraat <fraat87@gmail.com>
2021-03-23 21:30:05 +0100

    save

--- function.js
+++ function.js
@@ -11,90 +11,90 @@ function myFunction(imgs) {
     expandImg.parentElement.style.display = "block";
   }
 
-  randomNumber = Math.floor(Math.random()*4+1);
+  randomNumber = Math.floor(Math.random()*8+1);
 
 window.onload = function() {
 	if (randomNumber == 1) {
-		document.getElementById("news1").style.display = "inline";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
+		document.getElementById("fact1").style.display = "inline";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact7").style.display = "none";
+		document.getElementById("fact8").style.display = "none";
 	}
 	if (randomNumber == 2) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "inline";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "inline";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact7").style.display = "none";
+		document.getElementById("fact8").style.display = "none";
 	}
 	if (randomNumber == 3) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "inline";
-		document.getElementById("news4").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "inline";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact7").style.display = "none";
+		document.getElementById("fact8").style.display = "none";
 	}
 	if (randomNumber == 4) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "inline";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "inline";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact7").style.display = "none";
+		document.getElementById("fact8").style.display = "none";
 	}
 	if (randomNumber == 5) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "inline";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news3").style.display = "none";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "inline";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact7").style.display = "none";
+		document.getElementById("fact8").style.display = "none";
 		
 	}
 	if (randomNumber == 6) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "inline";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news3").style.display = "none";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "inline";
+		document.getElementById("fact7").style.display = "none";
+		document.getElementById("fact8").style.display = "none";
 		
 	}
 	if (randomNumber == 7) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "none";
-		document.getElementById("news5").style.display = "none";
-		document.getElementById("news6").style.display = "none";
-		document.getElementById("news8").style.display = "inline";
-		document.getElementById("news6").style.display = "none";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact7").style.display = "inline";
+		document.getElementById("fact8").style.display = "none";
 	}
 	if (randomNumber == 8) {
-		document.getElementById("news1").style.display = "none";
-		document.getElementById("news2").style.display = "none";
-		document.getElementById("news3").style.display = "none";
-		document.getElementById("news4").style.display = "none";
-		document.getElementById("news5").style.display = "none";
-		document.getElementById("news6").style.display = "none";
-		document.getElementById("news6").style.display = "none";
-		document.getElementById("news8").style.display = "inline";
+		document.getElementById("fact1").style.display = "none";
+		document.getElementById("fact2").style.display = "none";
+		document.getElementById("fact3").style.display = "none";
+		document.getElementById("fact4").style.display = "none";
+		document.getElementById("fact5").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact6").style.display = "none";
+		document.getElementById("fact8").style.display = "inline";
 		
 	}
 }
