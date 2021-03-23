03f7b057941a46222fc8258cf56a004d1d4c2469
fraat <fraat87@gmail.com>
2021-03-23 18:16:32 +0100

    save

--- function.js
+++ function.js
@@ -10,3 +10,32 @@ function myFunction(imgs) {
     // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
   }
+
+  randomNumber = Math.floor(Math.random()*4+1);
+
+window.onload = function() {
+	if (randomNumber == 1) {
+		document.getElementById("news1").style.display = "inline";
+		document.getElementById("news2").style.display = "none";
+		document.getElementById("news3").style.display = "none";
+		document.getElementById("news4").style.display = "none";
+	}
+	if (randomNumber == 2) {
+		document.getElementById("news1").style.display = "none";
+		document.getElementById("news2").style.display = "inline";
+		document.getElementById("news3").style.display = "none";
+		document.getElementById("news4").style.display = "none";
+	}
+	if (randomNumber == 3) {
+		document.getElementById("news1").style.display = "none";
+		document.getElementById("news2").style.display = "none";
+		document.getElementById("news3").style.display = "inline";
+		document.getElementById("news4").style.display = "none";
+	}
+	if (randomNumber == 4) {
+		document.getElementById("news1").style.display = "none";
+		document.getElementById("news2").style.display = "none";
+		document.getElementById("news3").style.display = "none";
+		document.getElementById("news4").style.display = "inline";
+	}
+}
